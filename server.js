59663c14275c065df8002046a805210a4a57e44b
Rizki Ardian <radeite8@gmail.com>
2017-10-12 20:54:00 +0700

    Added loggin to files using 'good' Added custom port capability

--- server.js
+++ server.js
@@ -1,7 +1,37 @@
 const Hapi = require('hapi')
+const port = process.env.PORT || 9000
+
 const server = new Hapi.Server()
-server.connection({ port: 9000 })
-server.register(require('inert'), (err) => {
+server.connection({ port })
+
+const getDate = () => {
+  const today = new Date
+  return `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`
+}
+
+const goodOptions = {
+  reporters: {
+    myFileReporter: [{
+      module: 'good-squeeze',
+      name: 'Squeeze',
+      args: [{ log: '*', error: '*' }]
+    }, {
+      module: 'good-squeeze',
+      name: 'SafeJson'
+    }, {
+      module: 'good-file',
+      args: [ __dirname + `/logs/${getDate()}.log` ]
+    }]
+  }
+}
+
+server.register([
+  {
+    register: require('good'),
+    options: goodOptions
+  },
+  require('inert')
+], (err) => {
   if (err) throw err
 })
 
